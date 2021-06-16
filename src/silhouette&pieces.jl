9d8265bc71e660701898862d148c8b630c14468a
toshiki-kosen <17204@fukushima.kosen-ac.jp>
2021-06-16 13:33:44 +0900

    Add silhouette&pieces.jl

---
+++ src/silhouette&pieces.jl
@@ -0,0 +1,7 @@
+tri_s = MYPolygon([0, 1, 1], [0, 0, 1])
+tri_m = MYPolygon([0, √2, √2], [0, 0, √2])
+tri_l = MYPolygon([0, 2, 2], [0, 0, 2])
+square_s = MYPolygon([0, 1, 1, 0], [0, 0, 1, 1])
+parallelogram = MYPolygon([0, √2, √(2)*3/2, √(2)/2], [0, 0, √(2)/2, √(2)/2])
+
+house2 = MYPolygon([0, 1, 1, 1.5, 0.5, -0.5, 0], [0, 0, 1, 1, 2, 1, 1])
