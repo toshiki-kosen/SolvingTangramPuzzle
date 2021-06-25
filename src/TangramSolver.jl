9052126924a949d4aa4dbf380c67d4b0f7f3d20c
toshiki-kosen <17204@fukushima.kosen-ac.jp>
2021-06-25 11:10:11 +0900

    Update loss function

--- src/TangramSolver.jl
+++ src/TangramSolver.jl
@@ -1,4 +1,4 @@
-using Base: Float16
+using Base: Float16, Float64
 include("PolygonBase.jl")
 include("ESBase.jl")
 include("silhouette&pieces.jl")
@@ -13,7 +13,7 @@ eggholder(x, y) = -(100*y + 47)*sin(sqrt(abs(50x + 100y + 47))) - 100x*sin(sqrt(
 eggholder(X::Array) = eggholder(X[1], X[2])
 
 # X[1つめのピースのx座標, 1つめのピースのy座標, １つめの回転角Θ, 2つめのx座標, ...]
-function loss_poly(X::Array; c::Float64 = 0.0)
+function loss_poly(X::Array; Ki::Float64 = 0.0, Kv::Float64 = 0.8)
     global pices, silhouette
 
     # silhouetteとpieceの共通部分の面積を求める
@@ -28,23 +28,35 @@ function loss_poly(X::Array; c::Float64 = 0.0)
         unioned = LibGEOS.union(unioned, MYPolygon2LibGEOS(tmp_p))
         sum_pieces += MYPolygon2LibGEOS(tmp_p) |> LibGEOS.area
     end
-
     tmp = LibGEOS.intersection(unioned, MYPolygon2LibGEOS(silhouette))
-
     A = LibGEOS.area(tmp) / LibGEOS.area(MYPolygon2LibGEOS(silhouette))
 
     # 各2pieceの共通部分を求める
     inter = 1.0 - LibGEOS.area(unioned) / sum_pieces
 
-    return -100 * (A - c * inter)
+    # 頂点の誤差成分 
+    Δv = 0.0
+    for i in 1:silhouette.n
+        Δmin = 100.0
+        for i in 1:length(pieces)
+            tmp_p = move(pieces[i], X[3i - 2], X[3i - 1])
+            rotate!(tmp_p, X[3i])
+            for j in 1:tmp_p.n
+                Δmin = min(Δmin, sum((silhouette.vertexes[:,i] - tmp_p.vertexes[:, j]).^2))
+            end
+        end
+        Δv = max(Δv, Δmin)
+    end
+
+    return -100A + 100Kv * Δv + 100Ki * inter
 end
 
-silhouette = rotate(house2, π/3)
+silhouette = house2
 pieces = [square_s, tri_m]
 
 # 初期化
 cmaes = init_CMAES(zeros(3 * length(pieces)), 1.0, 0)
-max_gen = 100
+max_gen = 128
 
 fitnesses_ave = Array{Float64, 1}()
 fitnesses_max = Array{Float64, 1}()
@@ -63,7 +75,7 @@ anim = @animate for gen in 1:max_gen
     end
 
     # 評価値
-    loss(x) = loss_poly(x, c=0.1)
+    loss(x) = loss_poly(x, Ki=0.1, Kv = 0.8)
     fitnesses = get_fitness(X, loss)
 
     # 表示用
