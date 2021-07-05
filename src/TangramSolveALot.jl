ab6fea22681f86884db00930a78fc8cbcbd672af
toshiki-kosen <17204@fukushima.kosen-ac.jp>
2021-07-06 08:52:02 +0900

    Add multi thread feature

--- src/TangramSolveALot.jl
+++ src/TangramSolveALot.jl
@@ -1,4 +1,6 @@
-using Base: Float64
+using Printf: Threads
+using Plots: Threads
+using ProgressMeter
 using Printf
 include("PolygonBase.jl")
 include("ESBase.jl")
@@ -14,7 +16,7 @@ eggholder(x, y) = -(100*y + 47)*sin(sqrt(abs(50x + 100y + 47))) - 100x*sin(sqrt(
 eggholder(X::Array) = eggholder(X[1], X[2])
 
 # X[1つめのピースのx座標, 1つめのピースのy座標, １つめの回転角Θ, 2つめのx座標, ...]
-function loss_poly(X::Array; Ki::Float64 = 0.0, Kv::Float64 = 0.8)
+function loss_poly(X::Array; Ks::Float64 = 1.0, Ki::Float64 = 0.0, Kv::Float64 = 0.8)
     global pices, silhouette
 
     # silhouetteとpieceの共通部分の面積を求める
@@ -49,54 +51,57 @@ function loss_poly(X::Array; Ki::Float64 = 0.0, Kv::Float64 = 0.8)
         Δv = max(Δv, Δmin)
     end
 
-    return -100A + 100Kv * Δv + 100Ki * inter
+    return -100Ks * A + 100Kv * Δv + 100Ki * inter
 end
 
-silhouette = house2
-pieces = [square_s, tri_m]
+silhouette = rect_s
+pieces = [parallelogram, tri_s, tri_s]
 
 # 初期化
-max_gen = 96
-sample_num = 256
+max_gen = 128
+sample_num = 64
 
 # 最終結果を保存するか否か
 save_results = true
 if save_results
     rm("outputs", force=true, recursive=true)
     mkdir("outputs")
-end
 
-best_fitnesses = Array{Float64, 1}()
-correct_time = 0
-for t in 1:sample_num
-    global best_fitnesses, correct_time
-    local cmaes
-    cmaes = init_CMAES(zeros(3 * length(pieces)), 1.0, 0)
-    fitnesses = zeros(cmaes.dim)
-    X = zeros((cmaes.dim, cmaes.λ))
-    rng = MersenneTwister(t)
+    best_fitnesses = Array{Float64, 1}()
+    correct_time = 0
+    p = Progress(sample_num)
+    for t in 1:sample_num
+        global best_fitnesses, correct_time
+        local cmaes
+        cmaes = init_CMAES(zeros(3 * length(pieces)), 1.0, 0)
+        fitnesses = zeros(cmaes.dim)
+        X = zeros((cmaes.dim, cmaes.λ))
+        rng = MersenneTwister(t)
 
-    for gen in 1:max_gen
-        # 個体生成
-        X = samplePopulation(cmaes, rng=rng)
+        for gen in 1:max_gen
+            # 個体生成
+            X = samplePopulation(cmaes, rng=rng)
 
-        # 回転角のパラメータを -π ~ π までに正規化
-        for j in 1:size(X)[2]
-            for i in 3:3:size(X)[1]
-                X[i, j] %= 2π - π
+            # 回転角のパラメータを -π ~ π までに正規化
+            for j in 1:size(X)[2]
+                for i in 3:3:size(X)[1]
+                    X[i, j] %= 2π - π
+                end
             end
+
+            # 評価値
+            loss(x) = loss_poly(x,Ks = 1.0, Ki=0.6, Kv=0.8)
+            fitnesses = get_fitness(X, loss)
+
+            # 更新
+            update!(cmaes, X, fitnesses, gen)
         end
+        push!(best_fitnesses, maximum(fitnesses))
 
-        # 評価値
-        loss(x) = loss_poly(x, Ki=0.0, Kv=0.8)
-        fitnesses = get_fitness(X, loss)
-
-        # 更新
-        update!(cmaes, X, fitnesses, gen)
-    end
-    push!(best_fitnesses, maximum(fitnesses))
-    correct_time += -best_fitnesses[end] > 97 ? 1 : 0
-    if save_results
+        if -best_fitnesses[end] > 97
+            correct_time += 1
+        end
+        
         # 表示用
         best_arg = argmin(fitnesses)
         best_pieces = Array{MYPolygon, 1}()
@@ -107,13 +112,54 @@ for t in 1:sample_num
         end
         display(silhouette, best_pieces...)
         savefig(@sprintf "outputs\\%02.0f_%d_results" -best_fitnesses[end] t)
+
+        next!(p)
+    end
+else
+    best_fitnesses = Array{Float64, 1}()
+    correct_time = Threads.Atomic{Int}(0)
+    p = Progress(sample_num÷8)
+    Threads.@threads for t in 1:sample_num
+        global best_fitnesses, correct_time
+        local cmaes
+        cmaes = init_CMAES(zeros(3 * length(pieces)), 1.0, 0)
+        fitnesses = zeros(cmaes.dim)
+        X = zeros((cmaes.dim, cmaes.λ))
+        rng = MersenneTwister(t)
+
+        for gen in 1:max_gen
+            # 個体生成
+            X = samplePopulation(cmaes, rng=rng)
+
+            # 回転角のパラメータを -π ~ π までに正規化
+            for j in 1:size(X)[2]
+                for i in 3:3:size(X)[1]
+                    X[i, j] %= 2π - π
+                end
+            end
+
+            # 評価値
+            loss(x) = loss_poly(x,Ks = 1.0, Ki=0.6, Kv=0.8)
+            fitnesses = get_fitness(X, loss)
+
+            # 更新
+            update!(cmaes, X, fitnesses, gen)
+        end
+        push!(best_fitnesses, maximum(fitnesses))
+
+        if -best_fitnesses[end] > 97
+            Threads.atomic_add!(correct_time, 1)
+        end
+        
+        if Threads.threadid() == 1
+            next!(p)
+        end
     end
 end
 
-pl = histogram(-best_fitnesses, bins = 40:5:100)
+pl = histogram(-best_fitnesses, bins = 40:5:100, lengend=false)
 
-println("correct prob: $(correct_time)/$(sample_num) = $(100correct_time/sample_num)%")
+println("correct prob: $(correct_time[])/$(sample_num) = $(100correct_time[]/sample_num)%")
 
-savefig(pl, "fitness_distribusion_$(sample_num)samples.png")
+savefig(pl, "fitness_distribusion_$(sample_num)samples_$(correct_time[])-corrects.png")
 Base.display(pl)
-
